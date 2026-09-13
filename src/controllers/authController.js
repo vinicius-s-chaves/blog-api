9e8789f1147842690a4b0a4f63c2e1e293994ca7
Vinicius <228245525+vinicius-s-chaves@users.noreply.github.com>
2026-09-13 13:33:15 -0300

    fix: read user password to validate

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,19 +6,18 @@ import CustomError from "../utils/CustomError.js"
 export const login = async(req, res, next) => {
     const { email, password } = req.body
     try {
-        const user = await prisma.user.findUnique({
-            where: { email },
-            select: {
-                id: true,
-                username: true,
-                email: true
-            }
-        })
+        const user = await prisma.user.findUnique({ where: { email } })
         if(!user) return next(new CustomError("Invalid Credentials", 400))
         const verifyPassword = await bcrypt.compare(password, user.password)
         if(!verifyPassword) return next(new CustomError("Invalid Credentials", 400))
 
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" })
+        const payload = {
+            id: user.id,
+            username: user.username,
+            email: user.email
+        }
+
+        const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" })
         res.json({
             message: "Authenticated",
             token

aaff86744c1e7427f54ea72e5256fac960b4582c
Vinicius <228245525+vinicius-s-chaves@users.noreply.github.com>
2026-09-14 15:45:53 -0300

    fix: delete all posts and comments related to the user at the time of deletion

---
+++ src/prisma/migrations/20260914183720_delete_comments_and_posts_cascade/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE "comments" DROP CONSTRAINT "comments_author_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "comments" DROP CONSTRAINT "comments_post_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "posts" DROP CONSTRAINT "posts_author_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
